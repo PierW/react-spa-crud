de12876fb5a70091b77e9ba1e93d3d02fcdd6493
Pierpaolo Wurzburger <pierpaolo.wurzburger@meetweb.it>
2025-07-04 17:06:08 +0200

    inserisco pagina 404

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import logo from '../assets/images/logo.png';
 
 const Navbar = () => {
+  const classLink = ({ isActive }) => isActive ? 'text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2' : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2';
 
     return (
     <nav className="bg-indigo-700 border-b border-indigo-500">
@@ -24,24 +25,24 @@ const Navbar = () => {
               </Link>
               <div className="md:ml-auto">
                 <div className="flex space-x-2">
-                  <Link
+                  <NavLink
                     to="/"
-                    className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                    className={classLink}
                   >
                     Home
-                  </Link>
-                  <Link
+                  </NavLink>
+                  <NavLink
                     to="/jobs"
-                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                    className={classLink}
                   >
                     Jobs
-                  </Link>
-                  <Link
+                  </NavLink>
+                  <NavLink
                     to="/add-job"
-                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                    className={classLink}
                   >
                     Add Job
-                  </Link>
+                  </NavLink>
                 </div>
               </div>
             </div>
