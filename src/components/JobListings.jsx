41e312b14c386a588e3b97b815f6a0e4a674256d
Pierpaolo Wurzburger <pierpaolo.wurzburger@meetweb.it>
2025-07-07 18:20:18 +0200

    miglioro indentazione

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -9,36 +9,34 @@ const JobLisings = ({isHome = false}) => {
   useEffect(() => {
     const fetchJobs = async () => {
       try {
-        const res = await fetch('http://localhost:3000/jobs');
+        const res = await fetch("http://localhost:3000/jobs");
         const data = await res.json();
         setJobs(data);
       } catch (error) {
-        console.log('Cisono degli errori', error);
+        console.log("Cisono degli errori", error);
       } finally {
         setLoading(false);
       }
-    }
-
+    };
     fetchJobs();
   }, []);
 
     return (
-    <section className="bg-blue-50 px-4 py-10">
+      <section className="bg-blue-50 px-4 py-10">
         <div className="container-xl lg:container m-auto">
           <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-            {isHome ? 'Recent Jobs' : 'All Jobs'}
+            {isHome ? "Recent Jobs" : "All Jobs"}
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            { loading ? 
-            (<h2>Loading...</h2>) : 
-            (<> 
-              {jobs.map((job) => {
-                return (
-                    <JobListing job={job} key={job.id}/>
-                  )
-                })
-              }
-             </>) }
+            {loading ? (
+              <h2>Loading...</h2>
+            ) : (
+              <>
+                {jobs.map((job) => {
+                  return <JobListing job={job} key={job.id} />;
+                })}
+              </>
+            )}
           </div>
         </div>
       </section>
