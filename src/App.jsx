dd2dc7affcc14cbc3e9144f38a2dc545dc781e05
Pierpaolo Wurzburger <pierpaolo.wurzburger@meetweb.it>
2025-07-11 18:34:05 +0200

    aggiungo richiesta post per aggiungere nuovo job

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,32 @@ import MainLayout from "./layouts/MainLayout";
 import JobsPage from "./pages/JobsPage";
 import NotFound from "./pages/NotFound";
 import JobPage from "./pages/JobPage";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout/>}>
-      <Route index element={<HomePage />} />
-      <Route path="/jobs" element={<JobsPage />} />
-      <Route path="/jobs/:id" element={<JobPage/>} />
-      <Route path="*" element={<NotFound />} />
-    </Route>
-  )
-)
+import AddJobPage from "./pages/AddJobPage";
 
 function App() {
+
+  const saveJob = async (job) => {
+    await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type' : 'application/json'
+      },
+      body: JSON.stringify(job)
+    });
+    return;
+  }
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout/>}>
+        <Route index element={<HomePage />} />
+        <Route path="/jobs" element={<JobsPage />} />
+        <Route path="/add-job" element={<AddJobPage addNewJob={saveJob} />} />
+        <Route path="/jobs/:id" element={<JobPage/>} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    )
+  )
  
   return (
     <RouterProvider router={router} />
